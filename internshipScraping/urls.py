7f4f6c9b094d93962c4c4c9be56cb533a4123e15
armii23 <armihasratyan@gmail.com>
2022-10-02 00:59:52 +0400

    Add data scraping functionality

--- internshipScraping/urls.py
+++ internshipScraping/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from webScraping.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('webScraping.urls')),
+    #path('post/<int:postId>/', post)
 ]
--- webScraping/views.py
+++ webScraping/views.py
@@ -1,3 +1,117 @@
+from datetime import date, datetime
+from django.views.generic.list import ListView
+from webScraping.models import *
+import requests
+from bs4 import BeautifulSoup
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+class ScrapeData:
+
+    @classmethod
+    def get_linkedin_data(cls):
+
+        keywords = Keyword.objects.all()
+        keyword_dict = cls.replace_space(keywords)
+        job_data = list()
+
+        for key, keyword in keyword_dict.items():
+            url = f"https://www.linkedin.com/jobs/search/?currentJobId=3290944523&geoId=103030111&keywords={keyword}%20internship&location=Armenia&refresh=true&pageNum=0"
+            response = requests.get(url)
+            soup = BeautifulSoup(response.content, "html.parser")
+            get_list = soup.select('ul.jobs-search__results-list li')
+            cat_id = Category.objects.get(id=key)
+
+            for item in get_list:
+                published_date = item.find("time", {"class": "job-search-card__listdate"}).get("datetime")
+                link = item.find("a", {"class": "base-card__full-link"})
+                company = item.find("a", {"class": "hidden-nested-link"})
+
+                if link is None or company is None or published_date is None:
+                    continue
+                # if published_date is date.today():
+                data = {
+                    "title": item.find("h3", class_="base-search-card__title").text,
+                    "link": link.get('href'),
+                    "company": company.text,
+                    "category": cat_id
+                }
+                job_data.append(data)
+
+        cls.store_data(job_data)
+        return job_data
+
+    @classmethod
+    def get_staff_data(cls):
+
+        url = "https://staff.am/en/jobs?JobsFilter%5Bcompany%5D=&JobsFilter%5Bkey_word%5D=&JobsFilter" \
+              "%5Bjob_candidate_level%5D=&JobsFilter%5Bcategory%5D=&JobsFilter%5Bjob_type%5D=&JobsFilter%5Bjob_type" \
+              "%5D%5B%5D=3&JobsFilter%5Bsalary%5D=&JobsFilter%5Bjob_term%5D=&JobsFilter%5Bjob_city%5D=&JobsFilter" \
+              "%5Bsort_by%5D=&&JobsFilter%5Bsort_by%5D=0"
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, "html.parser")
+        get_list = soup.select('div.list-view div.featured-job')
+        job_data = list()
+        cat_id = Category.objects.get(title='Uncategorized')
+
+        for item in get_list:
+            link = item.find("a")
+            title = item.find("p", {"class": "font_bold"})
+            company = item.find("p", {"class": "job_list_company_title"})
+            published_date = item.find("span", {"class": "formatted_date"}).text
+            website_url = "https://staff.am"
+            format_date = datetime.strptime(published_date, '%d %B %Y').strftime('%Y-%m-%d')
+
+            # if format_date == date.today():
+            data = {
+                "title": title.text,
+                "link": website_url + link.get("href"),
+                "company": company.text,
+                "category": cat_id
+            }
+            job_data.append(data)
+
+        cls.store_data(job_data)
+        return job_data
+
+    @classmethod
+    def replace_space(cls, keyword_list):
+        keyword_dict = dict()
+
+        for item in keyword_list:
+            key = item.category_id
+            keyword = str(item.keyword).replace(" ", "%20")
+            keyword_dict[key] = keyword
+
+        return keyword_dict
+
+    @classmethod
+    def store_data(cls, data_list):
+
+        if bool(data_list):
+            print(data_list)
+            for data in data_list:
+                Internship.objects.create(**data)
+            return True
+        return False
+
+
+class InternshipView(ListView):
+    model = Internship
+    template_name = "webScraping/internship_list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        current_time = datetime.now().strftime('%H:%M:%S')
+
+        if current_time == "01:00:00":
+            ScrapeData.get_linkedin_data()
+            ScrapeData.get_staff_data()
+
+        return context
+
+
+class CategoryListView(ListView):
+    model = Category
+    template_name = "webScraping/internship_list.html"
